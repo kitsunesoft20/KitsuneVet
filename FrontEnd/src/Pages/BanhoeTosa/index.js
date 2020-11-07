77772d2e1c2706fd1d9100d207f56ee06d848aeb
Pokuspock <danniel.carmo@gmail.com>
2020-11-07 19:42:46 -0300

    Ta indo tudo bem : )

--- FrontEnd/src/Pages/BanhoeTosa/index.js
+++ FrontEnd/src/Pages/BanhoeTosa/index.js
@@ -1,9 +1,87 @@
 
 import React from 'react'
 
+import './banhoetosa.css'
+import Cabecalho from '../../Components/Cabecalho';
+import Rodape from '../../Components/Rodape';
+
+import imagem1 from './imagens/semfundo02.png';
+import imagem2 from './imagens/semfundo03.png';
+
 export default function BanhoeTosa()  {
     return (
-            <h1>Banho e Tosa</h1>
+
+        <div className="inicioBeT">
+
+            <Cabecalho />
+    
+            <div className="containerBeT">
+    
+                <div className="subboxBeT">
+                    <img src={imagem1} width="300px" height="300px" />
+                </div>
+    
+                <div className="subboxBeT2">
+    
+                    <div className="titBeT"> 
+                        {'{'} Banho e Tosa {'}'}
+                    </div>
+
+                    <div className="boxBeT">
+    
+                        <div className="dsformulariosBeT">
+                            <h4> Banho: </h4>
+                            <h4> Tosa: </h4>
+                            <h4> Cuidados com as unhas: </h4>
+                            <h4> Cuidados com os dentes: </h4>
+                            <h4> Observação: </h4>
+                            <h4> Horario: </h4>
+                            <h4> Data de Atendimento: </h4>
+                           <h4> Pet: </h4>
+                        </div>
+    
+                        <form className="formulariosBeT">
+                            
+                            <input type="checkbox" />
+                            
+                            <input type="checkbox" />
+    
+                            <input type="checkbox" />
+                            
+                            <input type="checkbox" />
+                           
+                            <input type="text" />
+                            
+                            <input type="time" min="08:00" max="18:00" />
+                            
+                            <input type="date" />
+    
+                            <select name="tipodepet" placeholder="tipodepet">
+                                <option value="" disabled selected> Selecione uma opção </option>
+                                <option value="feminino"> Gato </option>
+                                <option value="masculino"> Cachorro </option>
+                            </select>
+    
+                        </form>
+    
+                    </div>
+    
+                    <div className="ana">
+                        <input type="submit" value="Agendar" />
+                    </div>
+    
+                </div> 
+                
+                <div className="subboxBeT3">
+                    <img src={imagem2} width="300px" height="300px" />
+                </div>
+    
+            </div>
+    
+            <Rodape />
+
+        </div>
+        
     );
 
 }
--- FrontEnd/src/Pages/Home/index.js
+++ FrontEnd/src/Pages/Home/index.js
@@ -15,44 +15,44 @@ export default function Home()  {
     return (
     <body>
 
-    <div classNameName="inicio">
+    <div className="iniciohome">
 
         <Cabecalho />
 
-        <div className="servicos">
+        <div className="servicoshome">
 
             <div className="titservicos"> {'{'} O que a Kitsune Vet faz {'}'} </div>
             <h2> Agende já um de nossos serviços! </h2>
 
             <div className="imgservicos">
 
-                <div className="subbox"> 
+                <div className="subboxH"> 
                     <img src={hospedagem} width="120px" height="120px" alt="NotFound"/>
-                    <div className="minibox"> <h3>Hospedagem</h3> </div>
+                    <div className="miniboxH"> <h3>Hospedagem</h3> </div>
                     <div className="A"> <Link to="/agendamentohospedagem"> <button type="button">Agendar</button> </Link> </div>
                 </div>
 
-                <div className="subbox"> 
+                <div className="subboxH"> 
                     <img src={vacina} width="120px" height="120px" alt="NotFound"/>
-                    <div className="minibox"> <h3>Vacinas</h3> </div>
+                    <div className="miniboxH"> <h3>Vacinas</h3> </div>
                     <div className="A"> <Link to="/agendamentovacina"> <button type="button">Agendar</button> </Link> </div>
                 </div>
 
-                <div className="subbox"> 
+                <div className="subboxH"> 
                     <img src={cirurgia} width="120px" height="120px" alt="NotFound"/>
-                    <div className="minibox"> <h3>Cirurgia</h3> </div>
+                    <div className="miniboxH"> <h3>Cirurgia</h3> </div>
                     <div className="A"> <Link to="/agendamentocirurgia" > <button type="button">Agendar</button> </Link> </div>
                 </div>
 
-                <div className="subbox">
+                <div className="subboxH">
                     <img src={exames} width="120px" height="120px" alt="NotFound"/>
-                    <div className="minibox"> <h3>Exames</h3> </div>
+                    <div className="miniboxH"> <h3>Exames</h3> </div>
                     <div className="A"> <Link to="/agendamentoexame" > <button type="button">Agendar</button> </Link> </div>
                 </div>
 
-                <div className="subbox">
+                <div className="subboxH">
                     <img src={banhotosa} width="120px" height="120px" alt="NotFound"/>
-                    <div className="minibox"> <h3>Banho e Tosa</h3></div>
+                    <div className="miniboxH"> <h3>Banho e Tosa</h3></div>
                     <div className="A"> <Link to="/agendamentobanhoetosa" > <button type="button">Agendar</button> </Link> </div>
                 </div>
 
